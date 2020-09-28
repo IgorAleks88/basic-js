3a307c50d1f3c72a5a38812c8e94b7e0511448cd
IgorAleks88 <igor.alexeyenko@gmail.com>
2020-09-28 16:08:43 +0300

    refactory: start solutions

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("arr is not an array");
+  } else {
+    let newArr = new Array();
+    for (let i = 0; i < arr.length; i++) {
+      switch (arr[i]) {
+        case "--discard-next":
+          i++;
+          break;
+        case "--discard-prev":
+          newArr.pop();
+          break;
+        case "--double-next":
+          newArr.push(arr[i + 1]);
+          break;
+        case "--double-prev":
+          break;
+        default:
+          newArr.push(arr[i]);
+      }
+    }
+  }
   // remove line with error and write your code here
 };
